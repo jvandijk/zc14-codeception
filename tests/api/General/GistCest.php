4130d0f7bb2c0385cb9eafd54779ad395c0c2bfc
Jeroen van Dijk <jeroen@enrise.com>
2014-09-29 21:31:11 +0200

    Add check for specific gist

--- tests/api/General/GistCest.php
+++ tests/api/General/GistCest.php
@@ -13,4 +13,13 @@ class GistCest
         $I->seeResponseCodeIs(200);
         $I->seeResponseIsJson();
     }
+
+    public function testGetGist(ApiTester $I)
+    {
+        $I->wantTo('see if the we can get a gist');
+        $I->haveHttpHeader('Accept', 'application/vnd.github.beta+json');
+        $I->sendGet('/gists/2c47c9d59f4a5214f0c3');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+    }
 }
